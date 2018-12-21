e36c3f61ea3a04f90b2dcd6a56fe46ca73a0ef8a
M.S. Arun <msarun003@gmail.com>
2018-12-21 07:44:20 +0530

    Updated base_dir

--- Main_Script.sh
+++ Main_Script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #This Script is to Mirror Online Ubuntu LTS Repository with Local Drive.
 #Script Name	: ubuntu_lts_repo.sh
-#Dated 			  : December 2018
-#Author 		  : M.S. Arun
-#Email 			  : amsarun003@gmail.com
-#Usage			  : ./ubuntu_lts_repo.sh
+#Dated 			: December 2018
+#Author 		: M.S. Arun (538880) / DCS-UNIX-OPS-Team
+#Email 			: arun.ms3@cognizant.com
+#Usage			: ./ubuntu_lts_repo.sh
 #Last Update	: December 2018
 
 
@@ -13,19 +13,21 @@
 
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin
 
+base_dir="/var/www/html/yum6"
+
 #Local Declarations.
 backup_directory="/var/ubuntu_localrepo"
 rm -rf "$backup_directory" > /dev/null 2>&1
 /bin/mkdir -p "$backup_directory"
 
-mkdir -p /var/www/html/yum6/ubuntu_lts/ubuntu/
+mkdir -p $base_dir/ubuntu_lts/ubuntu/
 
-distro_list=("trusty" "trusty-security" "trusty-updates" "xenial" "xenial-security" "xenial-updates" "bionic" "bionic-security" "bionic-updates")
+distro_list=("trusty" "trusty-security" "trusty-updates" "trusty-backports" "xenial" "xenial-security" "xenial-updates" "xenial-backports" "bionic" "bionic-security" "bionic-updates" "bionic-backports")
 repo_list=("main" "multiverse" "restricted" "universe")
 
 for ((i=0;i<"${#distro_list[*]}";i++)); do
 distro_name="${distro_list[$i]}"
-wget --mirror --no-parent --reject "*index.html*" http://archive.ubuntu.com/ubuntu/dists/$distro_name/ -P /var/www/html/yum6/ubuntu_lts/
+wget --mirror --no-parent --reject "*index.html*" http://archive.ubuntu.com/ubuntu/dists/$distro_name/ -P $base_dir/ubuntu_lts/
 for ((j=0;j<"${#repo_list[*]}";j++)); do
 repo_name="${repo_list[$j]}"
 mkdir -p "$backup_directory/$distro_name/$repo_name"
@@ -35,11 +37,11 @@ gunzip "$backup_directory/$distro_name/$repo_name/Packages.gz"
 grep "Filename" "$backup_directory/$distro_name/$repo_name/Packages" | awk '{print "http://archive.ubuntu.com/ubuntu/"$2}' > "$backup_directory/$distro_name/$repo_name/download_list.temp"
 cat "$backup_directory/$distro_name/$repo_name/download_list.temp" | awk 'NF' | awk '{$1=$1;print}' > "$backup_directory/$distro_name/$repo_name/download_list.txt"
 while read -r package_name; do
-wget --mirror --no-parent --reject "*index.html*" "$package_name" -P /var/www/html/yum6/ubuntu_lts/
+wget --mirror --no-parent --reject "*index.html*" "$package_name" -P $base_dir/ubuntu_lts/
 done < "$backup_directory/$distro_name/$repo_name/download_list.txt"
 done; done;
-ln -sfn /var/www/html/yum6/ubuntu_lts/archive.ubuntu.com/ubuntu/dists /var/www/html/yum6/ubuntu_lts/ubuntu/
-ln -sfn /var/www/html/yum6/ubuntu_lts/archive.ubuntu.com/ubuntu/pool /var/www/html/yum6/ubuntu_lts/ubuntu/
+ln -sfn $base_dir/ubuntu_lts/archive.ubuntu.com/ubuntu/dists $base_dir/ubuntu_lts/ubuntu/
+ln -sfn $base_dir/ubuntu_lts/archive.ubuntu.com/ubuntu/pool $base_dir/ubuntu_lts/ubuntu/
 rm -rf "$backup_directory" > /dev/null 2>&1
 
 
