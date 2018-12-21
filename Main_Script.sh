b3c92a20df39e9c7d9cb9a026a51ccc3fbf0704f
M.S. Arun <msarun003@gmail.com>
2018-12-21 07:48:35 +0530

    Update Main_Script.sh

--- Main_Script.sh
+++ Main_Script.sh
@@ -13,6 +13,7 @@
 
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin
 
+web_url="archive.ubuntu.com"
 base_dir="/var/www/html/yum6"
 
 #Local Declarations.
@@ -29,21 +30,21 @@ repo_list=("main" "multiverse" "restricted" "universe")
 #Main Program
 for ((i=0;i<"${#distro_list[*]}";i++)); do
 distro_name="${distro_list[$i]}"
-wget --mirror --no-parent --reject "*index.html*" http://archive.ubuntu.com/ubuntu/dists/$distro_name/ -P $base_dir/ubuntu_lts/
+wget --mirror --no-parent --reject "*index.html*" http://$web_url/ubuntu/dists/$distro_name/ -P $base_dir/ubuntu_lts/
 for ((j=0;j<"${#repo_list[*]}";j++)); do
 repo_name="${repo_list[$j]}"
 mkdir -p "$backup_directory/$distro_name/$repo_name"
-wget http://archive.ubuntu.com/ubuntu/dists/$distro_name/$repo_name/binary-amd64/Packages.gz -P "$backup_directory/$distro_name/$repo_name/"
-wget http://archive.ubuntu.com/ubuntu/dists/$distro_name/$repo_name/binary-i386/Packages.gz -P "$backup_directory/$distro_name/$repo_name/"
+wget http://$web_url/ubuntu/dists/$distro_name/$repo_name/binary-amd64/Packages.gz -P "$backup_directory/$distro_name/$repo_name/"
+wget http://$web_url/ubuntu/dists/$distro_name/$repo_name/binary-i386/Packages.gz -P "$backup_directory/$distro_name/$repo_name/"
 gunzip "$backup_directory/$distro_name/$repo_name/Packages.gz"
-grep "Filename" "$backup_directory/$distro_name/$repo_name/Packages" | awk '{print "http://archive.ubuntu.com/ubuntu/"$2}' > "$backup_directory/$distro_name/$repo_name/download_list.temp"
+grep "Filename" "$backup_directory/$distro_name/$repo_name/Packages" | awk '{print "http://$web_url/ubuntu/"$2}' > "$backup_directory/$distro_name/$repo_name/download_list.temp"
 cat "$backup_directory/$distro_name/$repo_name/download_list.temp" | awk 'NF' | awk '{$1=$1;print}' > "$backup_directory/$distro_name/$repo_name/download_list.txt"
 while read -r package_name; do
 wget --mirror --no-parent --reject "*index.html*" "$package_name" -P $base_dir/ubuntu_lts/
 done < "$backup_directory/$distro_name/$repo_name/download_list.txt"
 done; done;
-ln -sfn $base_dir/ubuntu_lts/archive.ubuntu.com/ubuntu/dists $base_dir/ubuntu_lts/ubuntu/
-ln -sfn $base_dir/ubuntu_lts/archive.ubuntu.com/ubuntu/pool $base_dir/ubuntu_lts/ubuntu/
+ln -sfn $base_dir/ubuntu_lts/$web_url/ubuntu/dists $base_dir/ubuntu_lts/ubuntu/
+ln -sfn $base_dir/ubuntu_lts/$web_url/ubuntu/pool $base_dir/ubuntu_lts/ubuntu/
 rm -rf "$backup_directory" > /dev/null 2>&1
 
 
